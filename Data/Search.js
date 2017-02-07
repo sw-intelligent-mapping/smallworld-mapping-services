bbe40406c93b79f285520402ab0119477409f1e8
MickyRunham <michael.runham@ge.com>
2017-02-07 14:11:54 +0000

    Add files via upload

--- Data/Search.js
+++ Data/Search.js
@@ -1,1 +1,1 @@
-define({r:0.5,st:'Stem',n:1,t:[0],u:['../Content/A_Navigation/Copyright/swCopyright.htm'],s:['$'],p:[['$',12],['enter',5],['open',13],['uaa',6]]});
+define({r:0.5,st:'Stem',n:1,t:[0],u:['../Content/A_Navigation/Copyright/swCopyright.htm'],s:['$'],p:[['$',12],['electric',17],['ol',12],['triggered',12]]});
